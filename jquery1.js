d8beffd18ef022e713d45a325c4fab28cc3bcae7
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-19 14:22:48 -0600

    added 2nd row boxes, update box functions

--- jquery1.js
+++ jquery1.js
@@ -1,8 +1,22 @@
 $(function() {
-  $('.toggleh2').bind('click', function() {
-    $(this)
-      .toggleClass('pink')
-      .removeClass('blue');
-  });
-});
-
+    $('.toggleh2').bind('click', function() {
+      $(this)
+        .toggleClass('pink')
+        .removeClass('blue');
+    });
+    $('.box:eq(1)').bind('click', function() {
+      $('h2').toggleClass('pink')
+    });
+    $('.box:first').bind('click', function() {
+      $(this).hide(500).show(750)
+    });
+    $('.box:eq(2)').bind('click', function() {
+      $(this).animate({bottom:'200px'}, 300).animate({bottom:'-200px'}, 300).animate({bottom:'0px'}, 300)
+    });
+    $('.box:eq(3)').bind('click', function() {
+      $(this).animate({height:'200px'}, 300).animate({height:'50px'}, 300)
+    });
+    $('.box:last').bind('click', function() {
+      $(this).slideUp(500).slideDown(1000)
+    });
+   });
